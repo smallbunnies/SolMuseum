87fd756fec0dbc90e3cb3cb6fb88d25b68db27f5
Ruizhi Yu <rz.yu@foxmail.com>
2024-11-19 22:50:02 +0800

    speed up pytests

--- SolMuseum/pde/gas/broken_pipe/test_rup/test_cha.py
+++ SolMuseum/pde/gas/broken_pipe/test_rup/test_cha.py
@@ -3,80 +3,80 @@ from Solverz import Model, Param, Var, made_numerical, fdae_solver, Opt, Eqn, Ti
 import pandas as pd
 from SolMuseum.pde.gas.broken_pipe import rupture_pipe
 
-# %%
-L = 51000
-T = 2000
-lam = 0.03
-dx = 50
-M = int(L / dx)
-idx_leak = int(M * 0.5)
-dt = 0.1471
-p0 = 6621246.69079594
-q0 = 14
-
-m = Model()
-m.D = Param('D', value=0.5901)
-S = np.pi * (m.D / 2) ** 2
-m.lam = Param('lam', lam)
-m.va = Param('va', value=340)
-m.p = Var('p', value=p0 * np.ones((M + 1)))
-m.q = Var('q', value=q0 * np.ones((M + 1)))
-
-m.__dict__.update(rupture_pipe(m.p,
-                               m.q,
-                               m.lam,
-                               m.va,
-                               m.D,
-                               S,
-                               dx,
-                               dt,
-                               M,
-                               '1',
-                               idx_leak,
-                               method='cha'))
-
-m.pb = Param('pb', p0)
-m.qb = Param('qb', q0)
-m.bd1 = Eqn('bd1', m.p[0] - m.pb)
-m.bd2 = Eqn('bd2', m.q[M] - m.qb)
-
-# %% solution
-sfdae, y0 = m.create_instance()
-nfdae, code = made_numerical(sfdae, y0, output_code=True, sparse=True)
-
-pbar = 0.101325e6
-pb0 = m.p.value[idx_leak]
-pa_t = [pb0, pb0, pbar, pbar]
-tseries = [0, 300, 300 + 10, 10 * 3600]
-nfdae.p['pa'] = TimeSeriesParam('pa',
-                                v_series=pa_t,
-                                time_series=tseries)
-
-sol = fdae_solver(nfdae, [0, T], y0, Opt(step_size=dt))
-# %%
-# import matplotlib.pyplot as plt
-#
-# plt.plot(sol.T, sol.Y['p'][:, 0])
-# plt.plot(sol.T, sol.Y['p'][:, -1])
-# plt.plot(sol.T, sol.Y['p'][:, idx_leak])
-# plt.show()
-#
-# plt.plot(sol.T, sol.Y['q'][:, 0])
-# plt.plot(sol.T, sol.Y['q'][:, -1])
-# plt.show()
-#
-# res = dict()
-# res['pout'] = sol.Y['p'][:, -1]
-# res['qin'] = sol.Y['q'][:, 0]
-# df = pd.DataFrame(res)
-#
-# with pd.ExcelWriter(f'data/res.xlsx', engine='openpyxl', mode='a') as writer:
-#     # Write each DataFrame to a different sheet
-#     df.to_excel(writer, sheet_name='cha')
-
 
 # %%
 def test_cha(shared_datadir):
+    # %%
+    L = 51000
+    T = 2000
+    lam = 0.03
+    dx = 50
+    M = int(L / dx)
+    idx_leak = int(M * 0.5)
+    dt = 0.1471
+    p0 = 6621246.69079594
+    q0 = 14
+
+    m = Model()
+    m.D = Param('D', value=0.5901)
+    S = np.pi * (m.D / 2) ** 2
+    m.lam = Param('lam', lam)
+    m.va = Param('va', value=340)
+    m.p = Var('p', value=p0 * np.ones((M + 1)))
+    m.q = Var('q', value=q0 * np.ones((M + 1)))
+
+    m.__dict__.update(rupture_pipe(m.p,
+                                   m.q,
+                                   m.lam,
+                                   m.va,
+                                   m.D,
+                                   S,
+                                   dx,
+                                   dt,
+                                   M,
+                                   '1',
+                                   idx_leak,
+                                   method='cha'))
+
+    m.pb = Param('pb', p0)
+    m.qb = Param('qb', q0)
+    m.bd1 = Eqn('bd1', m.p[0] - m.pb)
+    m.bd2 = Eqn('bd2', m.q[M] - m.qb)
+
+    # %% solution
+    sfdae, y0 = m.create_instance()
+    nfdae, code = made_numerical(sfdae, y0, output_code=True, sparse=True)
+
+    pbar = 0.101325e6
+    pb0 = m.p.value[idx_leak]
+    pa_t = [pb0, pb0, pbar, pbar]
+    tseries = [0, 300, 300 + 10, 10 * 3600]
+    nfdae.p['pa'] = TimeSeriesParam('pa',
+                                    v_series=pa_t,
+                                    time_series=tseries)
+
+    sol = fdae_solver(nfdae, [0, T], y0, Opt(step_size=dt))
+    # %%
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot(sol.T, sol.Y['p'][:, 0])
+    # plt.plot(sol.T, sol.Y['p'][:, -1])
+    # plt.plot(sol.T, sol.Y['p'][:, idx_leak])
+    # plt.show()
+    #
+    # plt.plot(sol.T, sol.Y['q'][:, 0])
+    # plt.plot(sol.T, sol.Y['q'][:, -1])
+    # plt.show()
+    #
+    # res = dict()
+    # res['pout'] = sol.Y['p'][:, -1]
+    # res['qin'] = sol.Y['q'][:, 0]
+    # df = pd.DataFrame(res)
+    #
+    # with pd.ExcelWriter(f'data/res.xlsx', engine='openpyxl', mode='a') as writer:
+    #     # Write each DataFrame to a different sheet
+    #     df.to_excel(writer, sheet_name='cha')
+
     df = pd.read_excel(shared_datadir / 'res.xlsx',
                        sheet_name='cha',
                        engine='openpyxl',
--- SolMuseum/pde/gas/test/test_cha.py
+++ SolMuseum/pde/gas/test/test_cha.py
@@ -3,72 +3,72 @@ from Solverz import Var, Param, Model, made_numerical, TimeSeriesParam, Rodas, E
 from SolMuseum.pde import ngs_pipe
 import pandas as pd
 
-# %% mdl
-L = 51000
-p0 = 6621246.69079594
-q0 = 14
-va = 340
-D = 0.5901
-S = np.pi * (D / 2) ** 2
-lam = 0.03
-dx = 500
-
-# def test_cha(datadir):
-dt = 1.4706
-M = int(L / dx)
-m1 = Model()
-m1.p = Var('p', value=p0 * np.ones((M + 1,)))
-m1.q = Var('q', value=q0 * np.ones((M + 1,)))
-
-m1.__dict__.update(ngs_pipe(m1.p,
-                            m1.q,
-                            lam,
-                            va,
-                            D,
-                            S,
-                            dx,
-                            dt,
-                            M,
-                            '1',
-                            method='cha'))
-
-T = 5 * 3600
-pb1 = 1e6
-pb0 = 6621246.69079594
-pb_t = [pb0, pb0, pb1, pb1]
-tseries = [0, 1000, 1000 + 10 * dt, T]
-m1.pb = TimeSeriesParam('pb',
-                        v_series=pb_t,
-                        time_series=tseries)
-m1.qb = Param('qb', q0)
-m1.bd1 = Eqn('bd1', m1.p[0] - m1.pb)
-m1.bd2 = Eqn('bd2', m1.q[M] - m1.qb)
-fdae, y0 = m1.create_instance()
-nfdae, code = made_numerical(fdae, y0, sparse=True, output_code=True)
-
-# %% solution
-sol = fdae_solver(nfdae, [0, T], y0, Opt(step_size=dt))
-
-
-# import matplotlib.pyplot as plt
-# plt.plot(sol.T, sol.Y['p'][:, 0])
-# plt.plot(sol.T, sol.Y['p'][:, -1])
-# plt.show()
-#
-# plt.plot(sol.T, sol.Y['q'][:, 0])
-# plt.plot(sol.T, sol.Y['q'][:, -1])
-# plt.show()
-#
-# res = dict()
-# res['pout'] = sol.Y['p'][:, -1]
-# res['qin'] = sol.Y['q'][:, 0]
-# df = pd.DataFrame(res)
-#
-# with pd.ExcelWriter(f'res0.xlsx', engine='openpyxl', mode='a') as writer:
-#     # Write each DataFrame to a different sheet
-#     df.to_excel(writer, sheet_name='cha')
 
 def test_cha(shared_datadir):
+    # %% mdl
+    L = 51000
+    p0 = 6621246.69079594
+    q0 = 14
+    va = 340
+    D = 0.5901
+    S = np.pi * (D / 2) ** 2
+    lam = 0.03
+    dx = 500
+
+    # def test_cha(datadir):
+    dt = 1.4706
+    M = int(L / dx)
+    m1 = Model()
+    m1.p = Var('p', value=p0 * np.ones((M + 1,)))
+    m1.q = Var('q', value=q0 * np.ones((M + 1,)))
+
+    m1.__dict__.update(ngs_pipe(m1.p,
+                                m1.q,
+                                lam,
+                                va,
+                                D,
+                                S,
+                                dx,
+                                dt,
+                                M,
+                                '1',
+                                method='cha'))
+
+    T = 5 * 3600
+    pb1 = 1e6
+    pb0 = 6621246.69079594
+    pb_t = [pb0, pb0, pb1, pb1]
+    tseries = [0, 1000, 1000 + 10 * dt, T]
+    m1.pb = TimeSeriesParam('pb',
+                            v_series=pb_t,
+                            time_series=tseries)
+    m1.qb = Param('qb', q0)
+    m1.bd1 = Eqn('bd1', m1.p[0] - m1.pb)
+    m1.bd2 = Eqn('bd2', m1.q[M] - m1.qb)
+    fdae, y0 = m1.create_instance()
+    nfdae, code = made_numerical(fdae, y0, sparse=True, output_code=True)
+
+    # %% solution
+    sol = fdae_solver(nfdae, [0, T], y0, Opt(step_size=dt))
+
+    # import matplotlib.pyplot as plt
+    # plt.plot(sol.T, sol.Y['p'][:, 0])
+    # plt.plot(sol.T, sol.Y['p'][:, -1])
+    # plt.show()
+    #
+    # plt.plot(sol.T, sol.Y['q'][:, 0])
+    # plt.plot(sol.T, sol.Y['q'][:, -1])
+    # plt.show()
+    #
+    # res = dict()
+    # res['pout'] = sol.Y['p'][:, -1]
+    # res['qin'] = sol.Y['q'][:, 0]
+    # df = pd.DataFrame(res)
+    #
+    # with pd.ExcelWriter(f'res0.xlsx', engine='openpyxl', mode='a') as writer:
+    #     # Write each DataFrame to a different sheet
+    #     df.to_excel(writer, sheet_name='cha')
+
     df = pd.read_excel(shared_datadir / 'res0.xlsx',
                        sheet_name='cha',
                        engine='openpyxl',
