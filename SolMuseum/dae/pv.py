3a9e22a770762d8745a07a544a2742e66ceea795
Ruizhi Yu <rz.yu@foxmail.com>
2025-01-08 11:18:15 +0800

    refactor: avoid pv namespace conflict

--- SolMuseum/dae/pv.py
+++ SolMuseum/dae/pv.py
@@ -2,43 +2,40 @@ from Solverz import Eqn, Ode, AliasVar, TimeSeriesParam, Param, Model, Saturatio
 from Solverz import Var, Abs
 from Solverz.sym_algebra.symbols import iVar, idx
 from Solverz.utilities.type_checker import is_integer, is_number
+from Solverz.num_api.Array import Array
 from sympy import re as real, im as imag
 import numpy as np
 from warnings import warn
+from .util import rename_mdl
 
 
 class pv:
 
     def __init__(self,
-                 ISC=19.6,
-                 IM=18,
-                 Radiation=1000,
-                 sref=1000,
-                 Ttemp_pv=25,
-                 UOC=864,
-                 UM=688,
+                 name='pv',
                  **kwargs):
+
+        self.name = name
+
         for key, value in kwargs.items():
-            if isinstance(value, list):
-                kwargs[key] = np.array(value)
-            elif is_number(value):
-                kwargs[key] = np.array(value).reshape((-1,))
+            if isinstance(value, list) or is_number(value):
+                kwargs[key] = Array(value, dim=1)
 
         # PV panel parameter
-        self.ISC = ISC
-        self.IM = IM
-        self.Radiation = Radiation
+        self.ISC = kwargs.get('ISC')
+        self.IM = kwargs.get('IM')
+        self.Radiation = kwargs.get('Radiation')
         # self.Sfluc = Sfluc
-        self.sref = sref
-        self.Ttemp_pv = Ttemp_pv
-        self.UOC = UOC
-        self.UM = UM
+        self.sref = kwargs.get('sref')
+        self.Ttemp = kwargs.get('Ttemp')
+        self.UOC = kwargs.get('UOC')
+        self.UM = kwargs.get('UM')
 
         # PV grid variable
-        self.ux_pv = kwargs.get('ux_pv')
-        self.uy_pv = kwargs.get('uy_pv')
-        self.ix_pv = kwargs.get('ix_pv')
-        self.iy_pv = kwargs.get('iy_pv')
+        self.ux = kwargs.get('ux')
+        self.uy = kwargs.get('uy')
+        self.ix = kwargs.get('ix')
+        self.iy = kwargs.get('iy')
 
         # circuit parameter
         self.kop = kwargs.get('kop')
@@ -74,10 +71,10 @@ class pv:
 
     def cal_P(self):
         # 先计算upv，ipv，
-        isc = self.ISC * self.Radiation / self.sref * (1 + 0.0025 * (self.Ttemp_pv - 25))
-        im = self.IM * self.Radiation / self.sref * (1 + 0.0025 * (self.Ttemp_pv - 25))
-        uoc = self.UOC * (1 - 0.00288 * (self.Ttemp_pv - 25)) * np.log(np.exp(1) + 0.5 * (self.Radiation / 1000 - 1))
-        um = self.UM * (1 - 0.00288 * (self.Ttemp_pv - 25)) * np.log(np.exp(1) + 0.5 * (self.Radiation / 1000 - 1))
+        isc = self.ISC * self.Radiation / self.sref * (1 + 0.0025 * (self.Ttemp - 25))
+        im = self.IM * self.Radiation / self.sref * (1 + 0.0025 * (self.Ttemp - 25))
+        uoc = self.UOC * (1 - 0.00288 * (self.Ttemp - 25)) * np.log(np.exp(1) + 0.5 * (self.Radiation / 1000 - 1))
+        um = self.UM * (1 - 0.00288 * (self.Ttemp - 25)) * np.log(np.exp(1) + 0.5 * (self.Radiation / 1000 - 1))
         if 1 - im / isc != 0:
             c2 = (um / uoc - 1) / np.log(1 - im / isc)
         else:
@@ -90,26 +87,26 @@ class pv:
 
     def pv_init(self):
         P = self.cal_P()
-        P_actual = self.ux_pv * self.ix_pv + self.uy_pv * self.iy_pv
+        P_actual = self.ux * self.ix + self.uy * self.iy
 
         if not np.all(np.isclose(P, P_actual, atol=1e-6, rtol=1e-4)):
-            warn(f"PV output and grid injection not match, with deviation {np.abs(P-P_actual)}. "
+            warn(f"PV output and grid injection not match, with deviation {np.abs(P - P_actual)}. "
                  "Please calculate PV output and perform the power flow first!")
 
-        ugrid_pv = self.ux_pv + 1j * self.uy_pv
+        ugrid_pv = self.ux + 1j * self.uy
         exp_j_theta = ugrid_pv / np.abs(ugrid_pv)
         usk_ctrl = ugrid_pv / exp_j_theta
         ud_pv = usk_ctrl.real
         uq_pv = usk_ctrl.imag
 
-        ig = self.ix_pv + 1j * self.iy_pv
+        ig = self.ix + 1j * self.iy
         ig_ctrl = ig / exp_j_theta
         id_pv = ig_ctrl.real
         iq_pv = ig_ctrl.imag
 
         self.iL = self.ipv
-        self.uix = self.ux_pv - self.lf * self.iy_pv
-        self.uiy = self.uy_pv + self.lf * self.ix_pv
+        self.uix = self.ux - self.lf * self.iy
+        self.uiy = self.uy + self.lf * self.ix
         uidq = (self.uix + 1j * self.uiy) / exp_j_theta
         self.uid = uidq.real
         self.uiq = uidq.imag
@@ -133,60 +130,62 @@ class pv:
     def mdl(self):
         self.pv_init()
 
+        name = self.name
+
         m = Model()
 
         # panel model
-        m.Radiation = Param('Radiation', self.Radiation)
-        m.ISC = Param("ISC", self.ISC)
-        m.Sfluc = TimeSeriesParam('Sfluc', [1, 1], [0, 3600])
-        m.sref = Param("sref", self.sref)
-        m.Ttemp_pv = Param("Ttemp_pv", self.Ttemp_pv)
-        m.IM = Param("IM", self.IM)
-        m.UOC = Param("UOC", self.UOC)
-        m.UM = Param("UM", self.UM)
+        m.Radiation = Param('Radiation_'+name, self.Radiation)
+        m.ISC = Param('ISC_'+name, self.ISC)
+        m.Sfluc = TimeSeriesParam('Sfluc_'+name, [1, 1], [0, 3600])
+        m.sref = Param('sref_'+name, self.sref)
+        m.Ttemp = Param('Ttemp_'+name, self.Ttemp)
+        m.IM = Param('IM_'+name, self.IM)
+        m.UOC = Param('UOC_'+name, self.UOC)
+        m.UM = Param('UM_'+name, self.UM)
 
-        isc = m.ISC * m.Radiation * m.Sfluc / m.sref * (1 + 0.0025 * (m.Ttemp_pv - 25))
-        im = m.IM * m.Radiation * m.Sfluc / m.sref * (1 + 0.0025 * (m.Ttemp_pv - 25))
-        uoc = m.UOC * (1 - 0.00288 * (m.Ttemp_pv - 25)) * ln(exp(1) + 0.5 * (m.Radiation * m.Sfluc / 1000 - 1))
-        um = m.UM * (1 - 0.00288 * (m.Ttemp_pv - 25)) * ln(exp(1) + 0.5 * (m.Radiation * m.Sfluc / 1000 - 1))
+        isc = m.ISC * m.Radiation * m.Sfluc / m.sref * (1 + 0.0025 * (m.Ttemp - 25))
+        im = m.IM * m.Radiation * m.Sfluc / m.sref * (1 + 0.0025 * (m.Ttemp - 25))
+        uoc = m.UOC * (1 - 0.00288 * (m.Ttemp - 25)) * ln(exp(1) + 0.5 * (m.Radiation * m.Sfluc / 1000 - 1))
+        um = m.UM * (1 - 0.00288 * (m.Ttemp - 25)) * ln(exp(1) + 0.5 * (m.Radiation * m.Sfluc / 1000 - 1))
         c2 = (um / uoc - 1) / ln(1 - im / isc)
         c1 = (1 - im / isc) * exp(-um / (c2 * uoc))
 
         # circuit model
-        m.kop = Param('kop', self.kop)
-        m.koi = Param('koi', self.koi)
-        m.ws = Param('ws', self.ws)
-        m.lf = Param('lf', self.lf)
-        m.kip = Param('kip', self.kip)
-        m.kii = Param('kii', self.kii)
-        m.Pnom = Param('Pnom', self.Pnom)
-        m.kp = Param('kp', self.kp)
-        m.ki = Param('ki', self.ki)
-        m.udcref = Param('udcref', self.udcref)
-        m.cpv = Param('cpv', self.cpv)
-        m.ldc = Param('ldc', self.ldc)
-        m.cdc = Param('cdc', self.cdc)
+        m.kop = Param('kop_'+name, self.kop)
+        m.koi = Param('koi_'+name, self.koi)
+        m.ws = Param('ws_'+name, self.ws)
+        m.lf = Param('lf_'+name, self.lf)
+        m.kip = Param('kip_'+name, self.kip)
+        m.kii = Param('kii_'+name, self.kii)
+        m.Pnom = Param('Pnom_'+name, self.Pnom)
+        m.kp = Param('kp_'+name, self.kp)
+        m.ki = Param('ki_'+name, self.ki)
+        m.udcref = Param('udcref_'+name, self.udcref)
+        m.cpv = Param('cpv_'+name, self.cpv)
+        m.ldc = Param('ldc_'+name, self.ldc)
+        m.cdc = Param('cdc_'+name, self.cdc)
 
         # perform initialization
-        m.ux_pv = Var('ux_pv', self.ux_pv)
-        m.uy_pv = Var('uy_pv', self.uy_pv)
-        m.ix_pv = Var('ix_pv', self.ix_pv)
-        m.iy_pv = Var('iy_pv', self.iy_pv)
-        m.udc = Var('udc', self.udc)
-        m.idref1 = Var('idref1', self.idref1)
-        m.urd1 = Var('urd1', self.urd1)
-        m.urq1 = Var('urq1', self.urq1)
-        m.upv = Var('upv', self.upv)
-        m.D1 = Var('D1', self.D1)
-        m.iL = Var('iL', self.iL)
+        m.ux = Var('ux_'+name, self.ux)
+        m.uy = Var('uy_'+name, self.uy)
+        m.ix = Var('ix_'+name, self.ix)
+        m.iy = Var('iy_'+name, self.iy)
+        m.udc = Var('udc_'+name, self.udc)
+        m.idref1 = Var('idref1_'+name, self.idref1)
+        m.urd1 = Var('urd1_'+name, self.urd1)
+        m.urq1 = Var('urq1_'+name, self.urq1)
+        m.upv = Var('upv_'+name, self.upv)
+        m.D1 = Var('D1_'+name, self.D1)
+        m.iL = Var('iL_'+name, self.iL)
 
-        ugrid_pv = m.ux_pv + 1j * m.uy_pv
+        ugrid_pv = m.ux + 1j * m.uy
         exp_j_theta = ugrid_pv / abs(ugrid_pv)
         usk_ctrl = ugrid_pv / exp_j_theta
         ud_pv = real(usk_ctrl)
         uq_pv = imag(usk_ctrl)
 
-        ig = m.ix_pv + 1j * m.iy_pv
+        ig = m.ix + 1j * m.iy
         ig_ctrl = ig / exp_j_theta
         id_pv = real(ig_ctrl)
         iq_pv = imag(ig_ctrl)
@@ -209,33 +208,28 @@ class pv:
         ipv = isc * (1 - c1 * (exp(m.upv / (c2 * uoc)) - 1))
         D = Saturation(m.kp * (um - m.upv) + m.ki * m.D1, 1e-6, 1 - 1e-6)
 
-        m.eqn_id = Ode('eqn_id', m.ws / m.lf * (uix - m.ux_pv + m.lf * m.iy_pv), m.ix_pv)
-        m.eqn_iq = Ode('eqn_iq', m.ws / m.lf * (uiy - m.uy_pv - m.lf * m.ix_pv), m.iy_pv)
-        m.eqn_idref1 = Ode('eqn_idref1', m.udcref - m.udc, m.idref1)
-        m.eqn_urd1 = Ode('eqn_urd1',
+        m.eqn_id = Ode('eqn_id_'+name, m.ws / m.lf * (uix - m.ux + m.lf * m.iy), m.ix)
+        m.eqn_iq = Ode('eqn_iq_'+name, m.ws / m.lf * (uiy - m.uy - m.lf * m.ix), m.iy)
+        m.eqn_idref1 = Ode('eqn_idref1_'+name, m.udcref - m.udc, m.idref1)
+        m.eqn_urd1 = Ode('eqn_urd1_'+name,
                          idref - id_pv,
                          m.urd1)
-        m.eqn_urq1 = Ode('eqn_urq1',
+        m.eqn_urq1 = Ode('eqn_urq1_'+name,
                          iqref - iq_pv,
                          m.urq1)
-        m.eqn_upv = Ode('eqn_upv',
+        m.eqn_upv = Ode('eqn_upv_'+name,
                         1 / m.cpv * (ipv - m.iL),
                         m.upv)
-        m.eqn_iL = Ode('eqn_iL',
+        m.eqn_iL = Ode('eqn_iL_'+name,
                        1 / m.ldc * (m.upv - (1 - D) * m.udc),
                        m.iL)
-        m.eqn_udc = Ode('eqn_udc',
+        m.eqn_udc = Ode('eqn_udc_'+name,
                         1 / m.cdc * ((1 - D) * m.iL - idc),
                         m.udc)
-        m.eqn_D1 = Ode('eqn_D1',
+        m.eqn_D1 = Ode('eqn_D1_'+name,
                        um - m.upv,
                        m.D1)
 
-        # artifact = dict()
-        # for key, value in m.__dict__:
-        #     if isinstance(value, (Var, Param, TimeSeriesParam, Eqn, Ode)):
-        #         artifact[key] = value
-        #
-        # return artifact
-        return m
+        m = rename_mdl(m, name)
 
+        return m
--- SolMuseum/dae/test/test_pv.py
+++ SolMuseum/dae/test/test_pv.py
@@ -2,10 +2,10 @@ from Solverz import made_numerical, Rodas, Opt, Eqn, TimeSeriesParam
 from ..pv import pv
 
 # pv_instance
-pi = pv(ux_pv=1.06592457,
-        uy_pv=0.18958303,
-        ix_pv=0.60628269,
-        iy_pv=0.1078317,
+pi = pv(ux=1.06592457,
+        uy=0.18958303,
+        ix=0.60628269,
+        iy=0.1078317,
         kop=-0.05,
         koi=-10,
         ws=376.99,
@@ -18,18 +18,25 @@ pi = pv(ux_pv=1.06592457,
         udcref=800,
         cpv=1e-4,
         ldc=0.05,
-        cdc=5e-3)
+        cdc=5e-3,
+        ISC=19.6,
+        IM=18,
+        Radiation=1000,
+        sref=1000,
+        Ttemp=25,
+        UOC=864,
+        UM=688, )
 
 pvmdl = pi.mdl()
-pvmdl.eqn_uxpv = Eqn('uxpv', pvmdl.ux_pv - 1.06592457)
-pvmdl.eqn_uypv = Eqn('uypv', pvmdl.uy_pv - 0.18958303)
+pvmdl.eqn_uxpv = Eqn('ux_pv', pvmdl.ux_pv - 1.06592457)
+pvmdl.eqn_uypv = Eqn('uy_pv', pvmdl.uy_pv - 0.18958303)
 
 sdae, y0 = pvmdl.create_instance()
 dae = made_numerical(sdae, y0, sparse=True)
 
-dae.p['Sfluc'] = TimeSeriesParam('Sfluc',
-                                 [1, 0.8, 0.8, 0.8],
-                                 [0, 0.00001, 0.00002, 20])
+dae.p['Sfluc_pv'] = TimeSeriesParam('Sfluc_pv',
+                                    [1, 0.8, 0.8, 0.8],
+                                    [0, 0.00001, 0.00002, 20])
 
 sol = Rodas(dae,
             [0, 20],
@@ -37,8 +44,12 @@ sol = Rodas(dae,
             Opt(pbar=True))
 
 import matplotlib.pyplot as plt
-plt.plot(sol.T, sol.Y['iL'])
+
+plt.plot(sol.T, sol.Y['iL_pv'])
 plt.show()
 
-plt.plot(sol.T, sol.Y['D1'])
+plt.plot(sol.T, sol.Y['D1_pv'])
+plt.show()
+
+plt.plot(sol.T, sol.Y['upv_pv'])
 plt.show()
